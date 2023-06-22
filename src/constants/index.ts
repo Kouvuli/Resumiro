f22d3bd3b63851885ada2c810b515ecc873aebc9
nmphung08 <nmphung08@gmail.com>
2023-06-23 02:08:34 +0700

    zkevm pull request

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,6 @@
-// import certificateAbi from "./Certificate.json";
-// import companyAbi from "./Company.json";
-// import experienceAbi from "./Experience.json";
-// import jobAbi from "./Job.json";
-// import resumeAbi from "./Resume.json";
-// import skillAbi from "./Skill.json";
-// import contractAddresses from "./contractAddressses.json"
+
 import userAbi from './User.json'
 import resumiroAbi from './Resumiro.json'
 import contractAddress from './contractAddress.json'
 
-// export { certificateAbi, companyAbi, experienceAbi, jobAbi, resumeAbi, skillAbi, userAbi };
 export { userAbi, resumiroAbi, contractAddress }
